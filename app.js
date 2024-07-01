76914d5814030b7608f54f60c6162024641bb4e7
david chinedu <chinedudavid241@gmail.com>
2024-07-01 02:08:39 +0100

    first commit

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const app = express();
 const weather = require('openweather-apis');
 
 
+
 const API_KEY = '0c273a9af104f75e3283ab76e83f77fc';
 weather.setAPPID(API_KEY);
 weather.setLang('en');
 weather.setUnits('metric');
-
-//const city = 'owerri'
-
-
 
 
 app.set('trust proxy', true);
@@ -19,17 +16,18 @@ app.listen(4000, ()=>{
     console.log("listening");
 })
 
-app.get("/h",(req, res)=>{
+app.get("/api/hello", async(req, res)=>{
 const {visitor_name}=req.query;
 if(visitor_name == "mark"){
-const clientip=req.ip
-fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=YOUR_API_KEY&ip=${clientip}`)
+const clientip='102.216.201.31'
+
+fetch(`http://ip-api.com/json/${clientip}`)
 .then((res)=>{
     return res.json();
 })
 .then((data)=>{
- //console.log(data)
-weather.setCity(data);
+ console.log(`data is :${data}`)
+weather.setCity(data.city);
 weather.getAllWeather((err, JSONObj) => {
     if (err) {
         console.error('Error fetching weather data:', err);
@@ -40,8 +38,8 @@ weather.getAllWeather((err, JSONObj) => {
     const weatherDescription = JSONObj.weather[0].description;
     res.send({
         "client_ip": clientip,
-  "location": data,
-  "greeting": `Hello, ${visitor_name}!, the temperature is ${temperature}degrees Celcius in ${data}`
+  "location": data.city,
+  "greeting": `Hello, ${visitor_name}!, the temperature is ${temperature}degrees Celcius in ${data.city} `
     })
 });
 
@@ -50,5 +48,3 @@ weather.getAllWeather((err, JSONObj) => {
 else{res.send("invalid query string. use this instead:  visitor_name='mark'")}
 
 })
-//0c273a9af104f75e3283ab76e83f77fc
-//    /api/hello?visitor_name="Mark"
